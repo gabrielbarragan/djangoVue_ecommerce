be677cef40effe8dcc6a31ee0b400ce25e1ad059
gabo <gabrielbarragan07@gmail.com>
2021-10-23 21:42:25 -0500

    Revert "terminada la configuración de django"
    This reverts commit 1caa036ceb52e012058c83464350a51c254bd464.

---
+++ beerup_django/beerup_django/urls.py
@@ -0,0 +1,11 @@
+"""beerup_django URL Configuration
+
+"""
+from django.contrib import admin
+from django.urls import path,  include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken'))
+]
---
+++ beerup_django/beerup_django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for beerup_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beerup_django.settings')
+
+application = get_wsgi_application()
