0ef45b742d22e03084bae5a9418b07211cb0ee91
gabo <gabrielbarragan07@gmail.com>
2021-11-09 05:52:17 -0500

    finished page and views for product details, created a django class based view, url for the view and vue component

--- beer_django_vue/src/views/Home.vue
+++ beer_django_vue/src/views/Home.vue
@@ -29,7 +29,7 @@
           <h3 class="is-size-4">{{ product.name }}</h3>
           <p class="is-size-6 has-text-grey">${{ product.price }}</p>
 
-          Ver Detalles
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4"> Ver Detalles </router-link>
         </div>
         </div>
     </div>
