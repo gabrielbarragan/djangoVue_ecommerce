33fcaf76276e525111f436fdc473e883c4a758a3
gabo <gabrielbarragan07@gmail.com>
2021-11-28 06:57:44 -0500

    se Creó la vista del carrito de compras y el componente Cart

--- beer_django_vue/src/router/index.js
+++ beer_django_vue/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
 
 const routes = [
   {
@@ -19,6 +20,16 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path:'/search',
+    name:'Search',
+    component: Search,
+  },
+  {
+    path:'/cart',
+    name:'Cart',
+    component: Cart,
+  },
+  {
     path:'/:category_slug/:product_slug',
     name:'Product',
     component:Product,
@@ -28,11 +39,7 @@ const routes = [
     name:'Category',
     component: Category,
   },
-  {
-    path:'/search',
-    name:'Search',
-    component: Search,
-  }
+
 ]
 
 const router = createRouter({
